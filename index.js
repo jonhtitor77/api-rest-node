84e41bdbb765ea83733229aa66519a4ed01f8677
jonatan vitale <jonatanomarvitale@gmail.com>
2025-11-29 14:33:56 -0300

    agregamos  no encontramos middleware

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from 'express';
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('bienvenido a mi API REST con Node.js y Express!');
+app.use((req, res, next) => {
+    // res.json({ message: 'API en mantenimiento' });
+    console.log(req.method);
+    next();
 });
+
+app.get('/', (req, res) => {
+  res.json({ message: "Bienvenido a nuestra API REST!" });
+});
+
+import noEncontrada from './src/middlewares/not-found.js';
+app.use(noEncontrada);
+// app.use((req, res,next) => {
+//     res.status(404).json({ error: "No Encontrada" });
+// });
 
 const PORT = 3000;
 
