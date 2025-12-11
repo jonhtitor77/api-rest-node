0199f3e2c65136a384b053b3ff100c6ce2cc6db0
jonatan vitale <jonatanomarvitale@gmail.com>
2025-12-10 23:10:20 -0300

    agregue error 500 de servidor y cambie el 402 a 400 como pedia el enunciado

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ import 'dotenv/config';
 
 
 import express from 'express';
+import cors from 'cors';
 
-// import cors from 'cors';
-// app.use(cors());
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: "Bienvenido a nuestra API REST!" });
 });
+
+
 
 import authRouter from './src/routes/auth.routes.js';
 app.use("/api/auth", authRouter);
@@ -25,6 +26,13 @@ import noEncontrada from './src/middlewares/not-found.js';
 app.use(noEncontrada);
 
 
+import errorHandler from './src/middlewares/error-handler.js';
+app.use(errorHandler);
+
+
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
+
---
+++ src/middlewares/error-handler.js
@@ -0,0 +1,12 @@
+export default (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Error interno del servidor' });
+
+
+const statusCode = err.status || 500;
+
+const mensage = statusCode === 500 ? 'Error interno del servidor' : err.message;
+
+res.status(statusCode).json({ message: mensage });
+};
+
